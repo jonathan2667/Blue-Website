880b20b4cd20db3640f9abc79c25adbb15222fcd
jonathan2667 <77002214+jonathan2667@users.noreply.github.com>
2021-01-22 16:12:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openFunction() {
+    document.getElementById("menu").style.width="80px";
+    document.getElementById("mainbox").style.marginRight="60px";
+}
+
+
+function closeFunction() {
+    document.getElementById("menu").style.width="40px";
+    document.getElementById("mainbox").style.marginRight="0px";
+}
